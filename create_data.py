fc24d79153c1fcdb5859e63e19d401dd4397861c
SoundarRavichandran <106737866+SoundarRavichandran@users.noreply.github.com>
2023-06-01 13:41:24 +0530

    Update create_data.py
    added pytest file

--- create_data.py
+++ create_data.py
@@ -1,6 +1,19 @@
 import os
+import logging as logger
 
-def test_generate_data():
-  print("Test Successful")
-  print(os.environ.get('Org_ID'))
-test_generate_data()
+org_id = os.environ('Org_ID')
+site_count = os.environ('Number_Of_Sites')
+node_distribution = os.environ('Node_Site_Distribution')
+tel_frequency = os.environ('Node_Telemetry_Frequency')
+
+# org_id = "some_org_id"
+# site_count = 2
+# node_distribution = {"site1": 4, "site2": 10}
+# tel_frequency = 30
+
+class TestCreateOrgSites:
+
+    def test_create_org_site(self):
+        logger.info(f"Variables are {org_id}, {site_count}, {node_distribution}, {tel_frequency}")
+
+

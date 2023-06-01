16a13accc6b60b03414b9699ed6e8dc0145a1299
SoundarRavichandran <106737866+SoundarRavichandran@users.noreply.github.com>
2023-06-01 13:46:26 +0530

    Update create_org_and_sites.py
    updated env cmd

--- create_org_and_sites.py
+++ create_org_and_sites.py
@@ -1,10 +1,10 @@
 import os
 import logging as logger
 
-org_id = os.environ('Org_ID')
-site_count = os.environ('Number_Of_Sites')
-node_distribution = os.environ('Node_Site_Distribution')
-tel_frequency = os.environ('Node_Telemetry_Frequency')
+org_id = os.environ['Org_ID']
+site_count = os.environ['Number_Of_Sites']
+node_distribution = os.environ['Node_Site_Distribution']
+tel_frequency = os.environ.get('Node_Telemetry_Frequency')
 
 # org_id = "some_org_id"
 # site_count = 2
